9323d2a6399bcef171793d33d47d7516effa2428
RichardArjoon <webdeveloper.rich@gmail.com>
2020-06-11 12:48:33 -0400

    Updates

---
+++ precache-manifest.e2e6f776fe23eb113312baf8fc360442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e0f4a5e34689735c1ca9832c472a77",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ac9d04807940682b418b",
+    "url": "/monster-rolodex/static/css/main.3523693f.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monster-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9d04807940682b418b",
+    "url": "/monster-rolodex/static/js/main.0e49f43b.chunk.js"
+  },
+  {
+    "revision": "b46281a00ff77db46658",
+    "url": "/monster-rolodex/static/js/runtime-main.c3b95803.js"
+  }
+]);
